7174ae6f14886a8f9ad905bed3ef02cd34acc827
Sean Purcell <sean.purcell@tapjoy.com>
2014-04-29 13:45:38 -0700

    view all media files, delete on tap

---
+++ src/com/badfortrains/filetransfer/MainActivity.java
@@ -0,0 +1,102 @@
+package com.badfortrains.filetransfer;
+
+import android.app.ActionBar.LayoutParams;
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+public class MainActivity extends ListActivity 
+		implements LoaderManager.LoaderCallbacks<Cursor> {
+
+	// This is the Adapter being used to display the list's data
+	SimpleCursorAdapter mAdapter;
+
+	// These are the Contacts rows that we will retrieve
+	static final String[] PROJECTION = new String[] {
+		MediaStore.Audio.Media._ID, MediaStore.Audio.Media.TITLE };
+
+	// This is the select criteria
+	static final String SELECTION =  MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+	private static final String TAG = "FileTest";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Create a progress bar to display while the list loads
+		ProgressBar progressBar = new ProgressBar(this);
+		progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+		progressBar.setIndeterminate(true);
+		getListView().setEmptyView(progressBar);
+
+		// Must add the progress bar to the root of the layout
+		ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+		root.addView(progressBar);
+
+		// For the cursor adapter, specify which columns go into which views
+		String[] fromColumns = { MediaStore.Audio.Media.TITLE };
+		int[] toViews = { android.R.id.text1 }; // The TextView in
+												// simple_list_item_1
+
+		Log.v(TAG, "STARTING");
+		// Create an empty adapter we will use to display the loaded data.
+		// We pass null for the cursor, then update it in onLoadFinished()
+		mAdapter = new SimpleCursorAdapter(this,
+				android.R.layout.simple_list_item_1, null, fromColumns,
+				toViews, 0);
+		setListAdapter(mAdapter);
+
+		// Prepare the loader. Either re-connect with an existing one,
+		// or start a new one.
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	// Called when a new Loader needs to be created
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// Now create and return a CursorLoader that will take care of
+		// creating a Cursor for the data being displayed.
+		return new CursorLoader(this, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+				PROJECTION, SELECTION, null, null);
+	}
+
+	// Called when a previously created loader has finished loading
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		// Swap the new cursor in. (The framework will take care of closing the
+		// old cursor once we return.)
+		mAdapter.swapCursor(data);
+	}
+
+	// Called when a previously created loader is reset, making the data
+	// unavailable
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// This is called when the last Cursor provided to onLoadFinished()
+		// above is about to be closed. We need to make sure we are no
+		// longer using it.
+		mAdapter.swapCursor(null);
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// Delete the item
+//		Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, 
+//			    id);
+//		this.getContentResolver().delete(uri, null, null);
+	}
+}
